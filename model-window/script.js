b0ab1439c97be23dea99fa0651fa07ba2828458a
Niamask <niamaskay@gmail.com>
2023-12-17 13:44:12 +0100

    some update

--- model-window/script.js
+++ model-window/script.js
@@ -10,18 +10,21 @@ function openModal() {
   // overlay.style.display = "block";
   model.classList.remove("hidden");
   overlay.classList.remove("hidden");
-  console.log(btn);
+  // console.log(btn);
 }
 
 function closeModal() {
-  model.style.display = "none";
-  overlay.style.display = "none";
+  // model.style.display = "none";
+  // overlay.style.display = "none";
+  model.classList.add("hidden");
+  overlay.classList.add("hidden");
 }
 
 console.log(btn.length);
 
 for (let i = 0; i < btn.length; i++) {
   btn[i].addEventListener("click", openModal);
+  console.log(btn[i].textContent);
 }
 
 modelClose.addEventListener("click", closeModal);
