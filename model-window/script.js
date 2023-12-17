a1ec09ed03095f716a8a617073a4c0b75bd9485f
Niamask <niamaskay@gmail.com>
2023-12-17 13:03:25 +0100

    Create model-window project

---
+++ model-window/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const btn = document.querySelectorAll(".show-modal");
+const overlay = document.querySelector(".overlay");
+const model = document.querySelector(".model");
+const modelClose = document.querySelector(".close-model");
+
+function openModal() {
+  model.style.display = "block";
+  overlay.style.display = "block";
+}
+
+function closeModal() {
+  model.style.display = "none";
+  overlay.style.display = "none";
+}
+
+console.log(btn.length);
+
+for (let i = 0; i < btn.length; i++) {
+  btn[i].addEventListener("click", openModal);
+}
+
+modelClose.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
